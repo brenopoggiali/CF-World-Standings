438d5798a6c681fd386fe0325bdbca9210f03d78
Breno Poggiali <brenopoggiali@gmail.com>
2020-03-29 17:38:45 -0300

    Add firefox version

---
+++ extension-firefox/js/background.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+	browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+		var tab = tabs[0];
+		var tab_url = tab.url;
+		var arg = tab_url.split("/");
+		var options = ["gym", "contest"]
+		if (options.includes(arg[3]) && arg[2] === "codeforces.com") {
+			$("#found").show();
+			$("#notFound").hide();
+		}
+		else {
+			$("#found").hide();
+			$("#notFound").show();
+		}
+	});
+})
+
+$("#goToStandings").click(() => {
+	browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+		var tab = tabs[0];
+		var tabUrl = tab.url;
+		var arg = tabUrl.split('/');
+		var listUrl = $("#selectedCountry").val();
+		if (listUrl === null) {
+			alert("Please select a country");
+		}
+		else {
+			var newUrl = "https://codeforces.com/" + arg[3] + "/" + arg[4] + "/standings?list=" + listUrl;
+			browser.tabs.update(tab.id, {url: newUrl});
+		}
+	});
+})
