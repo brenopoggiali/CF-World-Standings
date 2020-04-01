0f768a3a01fa50bfbb3d9d464529906e6e488c2f
Breno Poggiali <brenopoggiali@gmail.com>
2020-03-31 21:43:49 -0300

    Update deprecated functions and add storage to last used country

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-	chrome.tabs.getSelected(null, function(tab) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var tab = tabs[0];
 		var tab_url = tab.url;
 		var arg = tab_url.split("/");
 		var options = ["gym", "contest"]
+		chrome.storage.sync.get("lastCountryUrl", function(obj) {
+			if (obj.lastCountryUrl) $("#selectedCountry").val(obj.lastCountryUrl);
+		});
 		if (options.includes(arg[3]) && arg[2] === "codeforces.com") {
 			$("#found").show();
 			$("#notFound").hide();
@@ -15,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 $("#goToStandings").click(() => {
-	chrome.tabs.getSelected(null, function(tab) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var tab = tabs[0];
 		var tabUrl = tab.url;
 		var arg = tabUrl.split('/');
 		var listUrl = $("#selectedCountry").val();
@@ -24,6 +29,7 @@ $("#goToStandings").click(() => {
 		}
 		else {
 			var newUrl = "https://codeforces.com/" + arg[3] + "/" + arg[4] + "/standings?list=" + listUrl;
+			chrome.storage.sync.set({ "lastCountryUrl": listUrl })
 			chrome.tabs.update(tab.id, {url: newUrl});
 		}
 	});
